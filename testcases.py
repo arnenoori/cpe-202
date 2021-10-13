40ba0c79f048f3e5880a4ff98148296e6442a457
Arne <arnenoori@gmail.com>
2021-10-13 15:54:21 -0700

    Add files via upload

---
+++ testcases.py
@@ -0,0 +1,12 @@
+import unittest
+from bst import *
+
+class TestCases(unittest.TestCase):
+
+
+# test insertion, deletion, and every function etc.
+
+
+
+    if __name__ == '__main__':
+        unittest.main()
